241c5502331d722a3fc85be999c2d3ae08d38ec3
Tazo <tazogigitashvili@gmail.com>
2023-07-22 12:28:45 +0400

    Initial Commit

---
+++ YourBubble/YourBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourBubbleApp.swift
+//  YourBubble
+//
+//  Created by Tazo Gigitashvili on 22.07.23.
+//
+
+import SwiftUI
+
+@main
+struct YourBubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
